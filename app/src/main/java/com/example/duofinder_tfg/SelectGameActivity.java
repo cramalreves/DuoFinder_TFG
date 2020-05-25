b772d120dd1c3913f45af6c2ae41ea3ec271aa2c
abelsc24 <abelsc24@gmail.com>
2020-05-25 19:28:27 +0200

    nuevas activities

--- app/src/main/java/com/example/duofinder_tfg/SelectGameActivity.java
+++ app/src/main/java/com/example/duofinder_tfg/SelectGameActivity.java
@@ -10,5 +10,6 @@ public class SelectGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_game);
+        //hola
     }
 }
